c48d89604f21d11c7f3a373d2cb9f26e04bba475
Pradeep Arkasali <apradeep7@gmail.com>
2023-04-09 16:48:04 +0530

    Added Folders with Events, Commands in Domain.Core

---
+++ RMQMicroservice.Domain.Core/RMQMicroservice.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using RMQMicroservice.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMQMicroservice.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent>:IEventHandler where TEvent:Event 
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
